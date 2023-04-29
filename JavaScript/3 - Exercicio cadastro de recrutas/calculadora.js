fa3f2315640d3b76225df1da3244251b27526a70
Bruno Costa <brunoleonardofc@gmail.com>
2023-04-29 15:31:03 -0400

    update java

---
+++ JavaScript/3 - Exercicio cadastro de recrutas/calculadora.js
@@ -0,0 +1,17 @@
+const n1 = prompt ("Informe o primeiro numero")
+const n2 = prompt ("Informe o segundo numero:")
+
+const soma = n1+n2
+const subtracao= n1-n2
+const multiplicacao = n1*n2
+const divisao = n1/n2
+
+alert (
+  "O resultado da soma entre esses numeros é:"\n +
+  "\n Soma " + soma
+  "\n Subtracao" + subtracao
+  "\n multiplicacao" + multiplicacao
+  "\n divisao" + divisao
+
+
+)
