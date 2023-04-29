804819eca702cf2ac8f90d8e8d0a6d57044967f6
Bruno Costa <brunoleonardofc@gmail.com>
2023-04-29 14:34:24 -0400

    create exercicio recruta

---
+++ JavaScript/3 - Exercicio cadastro de recrutas/cadastro.js
@@ -0,0 +1,11 @@
+const primeiroNome = prompt ("Informe o primeiro nome:")
+const sobrenome = prompt ("Informe o sobrenome:")
+const campoDeEstudo = prompt ("Qual é o seu campo de estudo?")
+const anoDeNascimento = prompt ("Qual o seu ano de nascimento?")
+
+alert (
+  "Recruta cadastrado com Sucesso!" +
+  "Nome completo:" + primeiroNome + " " + sobrenome +
+  "campoDeEstudo:" + campoDeEstudo
+  "Idade:" + (2023-anoDeNascimento)
+)
