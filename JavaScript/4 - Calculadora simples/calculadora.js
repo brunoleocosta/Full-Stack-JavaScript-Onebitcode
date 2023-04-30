648ea52fd4ebac72167bde86ca58144b076ac046
Bruno Costa <brunoleonardofc@gmail.com>
2023-04-30 09:44:04 -0400

    correcao calculadora

--- JavaScript/4 - Calculadora simples/calculadora.js
+++ JavaScript/4 - Calculadora simples/calculadora.js
@@ -1,18 +1,18 @@
-const n1 = prompt ("Informe o primeiro número:")
-const n2 = prompt ("Informe o segundo número:")
+const n1 = prompt("Informe o primeiro número:");
+const n2 = prompt("Informe o segundo número:");
 
-const n1 = parseFloat(n1)
-const n2 = parseFloat(n2)
+const parsedN1 = parseFloat(n1);
+const parsedN2 = parseFloat(n2);
 
-const soma = x+y
-const subtracao = x-y
-const multiplicacao = x*
-const divisao = x/y
+const soma = parsedN1 + parsedN2;
+const subtracao = parsedN1 - parsedN2;
+const multiplicacao = parsedN1 * parsedN2;
+const divisao = parsedN1 / parsedN2;
 
 alert(
   "Resultados:\n" + 
-  "\nsome: " + soma + 
+  "\nsoma: " + soma + 
   "\nsubtracao: " + subtracao + 
-  "\nmultiplicacao: " multiplicacao +
+  "\nmultiplicacao: " + multiplicacao +
   "\ndivisao: " + divisao
-)
+);
