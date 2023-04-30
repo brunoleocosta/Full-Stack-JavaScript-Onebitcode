db8a6f9be4649ae692693a7c9250d50997b93903
Bruno Costa <brunoleonardofc@gmail.com>
2023-04-29 20:50:16 -0400

    calculadora simples

--- JavaScript/3 - Exercicio cadastro de recrutas/name.js
+++
@@ -1,4 +0,0 @@
-const prompt = required ('prompt-sync')();
-var fname = prompt ('Enter your first name');
-var lname = prompt ('Enter your last name');
-console.log ("your full name is:",fname,fname,);
---
+++ JavaScript/4 - Calculadora simples/calculadora.js
@@ -0,0 +1,18 @@
+const n1 = prompt ("Informe o primeiro número:")
+const n2 = prompt ("Informe o segundo número:")
+
+const n1 = parseFloat(n1)
+const n2 = parseFloat(n2)
+
+const soma = x+y
+const subtracao = x-y
+const multiplicacao = x*
+const divisao = x/y
+
+alert(
+  "Resultados:\n" + 
+  "\nsome: " + soma + 
+  "\nsubtracao: " + subtracao + 
+  "\nmultiplicacao: " multiplicacao +
+  "\ndivisao: " + divisao
+)
