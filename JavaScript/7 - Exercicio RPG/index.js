9c5a5dbebddbb7e868507cdb981d5aa8358b9243
Bruno Costa <brunoleonardofc@gmail.com>
2023-05-05 06:01:34 -0400

    update

---
+++ JavaScript/7 - Exercicio RPG/index.js
@@ -0,0 +1,21 @@
+const nomePersonagem1 = prompt("Digite o nome o nome do personagem 1:");
+const atk1 = Number(prompt("Digite o poder de ataque do seu personagem 1"));
+
+const nomePersonagem2 = prompt("Digite o nome o nome do personagem 2:");
+const life2 = Number(prompt("Digite a quantidade de pontos de vida do seu personagem 2"));
+const def2 = Number(prompt("Digite o poder de defesa do personagem 2"));
+const shield2 = Number(prompt("Se tiver escudo, digite o valor, senão considere e digite 0"));
+
+const damage1 = atk1-def2
+const damage2 = atk1-def2-shield2
+
+
+if (atk1 > def2 && shield2 === 0) {
+    alert ("O dano foi de: " + damage1);
+}
+else if (atk1 > def2 && shield2 !== 0) {
+    alert ("O dano foi de: " + damage2);
+}
+else if (atk1 < def2 ) {
+    alert ("O dano foi de: 0");
+}
